b67761cb4fb5ef31238c0bd2c8d497fc109f7cef
RGAA-Software <hollow.ichigo.yoo@gmail.com>
2023-09-06 13:45:55 +0800

    delete useless code
    Signed-off-by: RGAA-Software <hollow.ichigo.yoo@gmail.com>

--- src/capture/CursorCapture.cpp
+++ src/capture/CursorCapture.cpp
@@ -17,9 +17,6 @@ namespace rgaa {
     }
 
     std::shared_ptr<NetMessage> CursorCapture::Capture() {
-        static HCURSOR cursor_hand = LoadCursorW(NULL, IDC_HAND);
-        static HCURSOR cursor_ibeam = LoadCursorW(NULL, IDC_IBEAM);
-
         CURSORINFO cursor_info = { 0 };
         cursor_info.cbSize = sizeof(CURSORINFO);
         GetCursorInfo(&cursor_info);
@@ -55,32 +52,6 @@ namespace rgaa {
         info->set_tex_right(std::get<2>(position));
 
         return msg;
-//        if (cursor_callback && width > 0 && height > 0 && cursor_data.size() > 0) {
-//            auto data = Data::Make(cursor_data.data(), cursor_data.size());
-//            auto cursor_info_msg = std::make_shared<CursorInfoMessage>();
-//            cursor_info_msg->data = data;
-//            cursor_info_msg->width = width;
-//            cursor_info_msg->height = height;
-//            cursor_info_msg->x = x;
-//            cursor_info_msg->y = y;
-//            if (capture_monitor) {
-//                cursor_info_msg->x_in_self_monitor = x - capture_monitor->left;
-//                cursor_info_msg->y_in_self_monitor = y;
-//                cursor_info_msg->screen_width = capture_monitor->width;
-//                cursor_info_msg->screen_height = capture_monitor->height;
-//                cursor_info_msg->xf = cursor_info_msg->x_in_self_monitor * 1.0f / cursor_info_msg->screen_width;
-//                cursor_info_msg->yf = cursor_info_msg->y_in_self_monitor * 1.0f / cursor_info_msg->screen_height;
-//            }
-//
-//            if (cursor_hand == cursor_info.hCursor) {
-//                cursor_info_msg->type = rgaa::CursorType::kIDC_HAND;
-//            }
-//            else if (cursor_ibeam == cursor_info.hCursor) {
-//                cursor_info_msg->type = rgaa::CursorType::kIDC_IBEAM;
-//            }
-//            cursor_callback(cursor_info_msg);
-//        }
-        return nullptr;
     }
 
 }
